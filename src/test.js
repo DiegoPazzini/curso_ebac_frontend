96454a11150101b12e38a922b0f8dbb3ffb3763e
Diego Pazzini <diegopazzini2009@hotmail.com>
2023-08-14 21:14:27 -0300

    Add files via upload

---
+++ src/test.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplica(a, b) {
+    return a * b;
+}
+function dizOla(nome) {
+    return `Olá ${nome}`;
+}
+console.log(multiplica(20, 5));
+console.log(dizOla('Diego Pazzini'));
---
+++ src/test.ts
@@ -0,0 +1,12 @@
+function multiplica(a: number, b: number){
+    return  a * b;
+}
+
+
+function dizOla(nome: string){
+    return `Olá ${nome}`
+}
+
+console.log(multiplica(20, 5))
+console.log(dizOla('Diego Pazzini'))
+
